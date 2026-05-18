a0078686651b7e98f8394b4a89e3bd3140d8a6c2
LDG1599 <yes388087@gmail.com>
2026-05-18 11:43:32 +0900

    031

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -8,15 +8,13 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @Controller
@@ -39,9 +37,10 @@ public class PostController {
         @Size(min = 2, max = 20, message = "02-title-제목은 2자 이상, 20자 이하로 입력가능합니다.")
         private String title;
         @NotBlank(message = "03-content-내용을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "04-content-내용은 2자 이상, 20자 이하로 입력가능합니다.")
+        @Size(min = 2, max = 100, message = "04-content-내용은 2자 이상, 100자 이하로 입력가능합니다.")
         private String content;
     }
+
 
     @GetMapping("/posts/write")
 
@@ -49,19 +48,46 @@ public class PostController {
         return "post/post/write";
     }
 
-    @PostMapping("/posts/doWrite")
+    @PostMapping("/posts/write")
     @Transactional
     public String write(
             @ModelAttribute("form")  @Valid WriteForm form,
-            BindingResult bindingResult,
-            Model model
+            BindingResult bindingResult
     ) {
         if (bindingResult.hasErrors()) return "post/post/write";
 
         Post post = postService.write(form.getTitle(), form.getContent());
 
+        return "redirect:/posts/" + post.getId();
+    }
+
+
+    @GetMapping("/posts/{id}")
+    @Transactional(readOnly = true)
+    public String showDetail(
+            @PathVariable int id,
+            Model model
+    ) {
+        Post post = postService.findById(id).get();
+
         model.addAttribute("post", post);
 
-        return "post/post/writeDone";
+        return "post/post/detail";
+    }
+
+
+    @GetMapping("/posts")
+    @Transactional(readOnly = true)
+    public String showList(Model model) {
+        List<Post> posts = postService.findAll();
+
+        model.addAttribute("posts", posts);
+
+        return "post/post/list";
+    }
+
+    @GetMapping("/posts/")
+    public String redirectToList() {
+        return "redirect:/posts";
     }
 }
